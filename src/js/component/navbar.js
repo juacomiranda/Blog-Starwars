6c894ff516c9f262f67668445cd2c504904884ec
juacomiranda <jmiranda@live.cl>
2024-06-14 03:06:30 -0400

    inicio

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+			<nav className="navbar navbar-dark bg-dark mb-3 px-5">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">Blog de Starwars</span>
 				</Link>
-			</div>
-		</nav>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Favoritos</button>
+					</Link>
+				</div>
+			</nav>
 	);
 };

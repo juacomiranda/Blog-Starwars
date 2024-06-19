c8d1f5f3cb93c6fa258203f7442bcb268545f460
juacomiranda <jmiranda@live.cl>
2024-06-19 11:04:20 -0400

    cardpersonajes

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -2,11 +2,13 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 export const Card = () => (
-    <div className="card" style={{ width: '18rem' }}>
-        <img src={rigoImage} className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">Personajes</h5>
-            <a href="#" className="btn btn-danger">Saber más</a>
-        </div>
-    </div>
+    <div className="col">
+            <div className="card h-100">
+                <img src={rigoImage} className="card-img-top" alt="personajes" />
+                <div className="card-body">
+                    <h5 className="card-title">Nombre</h5>
+                    <a href="#" className="btn btn-danger">Saber más</a>
+                </div>
+            </div>   
+            </div>
 );
--- src/js/views/personas.js
+++ src/js/views/personas.js
@@ -6,7 +6,14 @@ export const Personajes = () => {
   return (  
     <div className="container flex-fill text-center">
         <h1>Personajes</h1>
-        <Card />
+        <div className="row row-cols-1 row-cols-md-3 g-4 mt-1 mb-5">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+        </div>
     </div>
   );
 };
